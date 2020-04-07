1180c1bee929fda5cb0dfdbcefe6f459bb3c5a77
Dan Helgeland <dan.helgeland@gmail.com>
2020-04-07 19:35:51 +0200

    made text file demo

---
+++ Csharp/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Url { get; set; }
+
+    }
+}
--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Csharp
 {
@@ -6,9 +9,55 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\Demos\Test.txt";
 
-            //TODO: lag plan for prosjektet.
-            Console.WriteLine("Hello World!");
+            //List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            //Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (var line in lines)
+            {
+                string[] entries = line.Split(',');
+
+                Person newPerson = new Person();
+
+                newPerson.FirstName = entries[0];
+                newPerson.LastName = entries[1];
+                newPerson.Url = entries[2];
+
+
+                people.Add(newPerson);
+            }
+
+            foreach (var Person in people)
+            { 
+                Console.WriteLine($"{Person.FirstName}{Person.LastName}: {Person.Url} ");
+            }
+
+            people.Add(new Person { FirstName = "Greg", LastName = "Jones", Url = "www.test.no" });
+
+            List<string> output = new List<string>();
+
+            foreach (var person in people)
+            {
+                output.Add($"{ person.FirstName },{ person.LastName }, { person.Url}");
+            }
+
+            Console.WriteLine("write to text file");
+
+            File.WriteAllLines(filePath, output);
+            Console.WriteLine("All entries written");
+
         }
+
     }
 }
